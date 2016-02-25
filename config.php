0dedabaaaa9e389e6ea5c8034aadcf98c7f0b596
Dima Budko <dimonich41@gmail.com>
2016-02-25 21:13:53 +0200

    Init commit, basic template of OC

---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://e-shop/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://e-shop/');
+
+// DIR
+define('DIR_APPLICATION', 'Z:/home/e-shop/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/e-shop/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/e-shop/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/e-shop/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/e-shop/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/e-shop/www/image/');
+define('DIR_CACHE', 'Z:/home/e-shop/www/system/storage/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/e-shop/www/system/storage/download/');
+define('DIR_LOGS', 'Z:/home/e-shop/www/system/storage/logs/');
+define('DIR_MODIFICATION', 'Z:/home/e-shop/www/system/storage/modification/');
+define('DIR_UPLOAD', 'Z:/home/e-shop/www/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'e-shop');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
