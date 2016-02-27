71f46dd6df82f93fbcb633d5b3b1ccd16ededcdc
Dima Budko <dimonich41@gmail.com>
2016-02-27 17:26:46 +0200

    Added languages to header.

--- admin/config.php
+++ admin/config.php
@@ -1,25 +1,25 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://e-shop2/admin/');
-define('HTTP_CATALOG', 'http://e-shop2/');
+define('HTTP_SERVER', 'http://e-shop/admin/');
+define('HTTP_CATALOG', 'http://e-shop/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://e-shop2/admin/');
-define('HTTPS_CATALOG', 'http://e-shop2/');
+define('HTTPS_SERVER', 'http://e-shop/admin/');
+define('HTTPS_CATALOG', 'http://e-shop/');
 
 // DIR
-define('DIR_APPLICATION', 'Z:/home/e-shop2/www/admin/');
-define('DIR_SYSTEM', 'Z:/home/e-shop2/www/system/');
-define('DIR_LANGUAGE', 'Z:/home/e-shop2/www/admin/language/');
-define('DIR_TEMPLATE', 'Z:/home/e-shop2/www/admin/view/template/');
-define('DIR_CONFIG', 'Z:/home/e-shop2/www/system/config/');
-define('DIR_IMAGE', 'Z:/home/e-shop2/www/image/');
-define('DIR_CACHE', 'Z:/home/e-shop2/www/system/cache/');
-define('DIR_DOWNLOAD', 'Z:/home/e-shop2/www/system/download/');
-define('DIR_UPLOAD', 'Z:/home/e-shop2/www/system/upload/');
-define('DIR_LOGS', 'Z:/home/e-shop2/www/system/logs/');
-define('DIR_MODIFICATION', 'Z:/home/e-shop2/www/system/modification/');
-define('DIR_CATALOG', 'Z:/home/e-shop2/www/catalog/');
+define('DIR_APPLICATION', 'Z:/home/e-shop/www/admin/');
+define('DIR_SYSTEM', 'Z:/home/e-shop/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/e-shop/www/admin/language/');
+define('DIR_TEMPLATE', 'Z:/home/e-shop/www/admin/view/template/');
+define('DIR_CONFIG', 'Z:/home/e-shop/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/e-shop/www/image/');
+define('DIR_CACHE', 'Z:/home/e-shop/www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/e-shop/www/system/download/');
+define('DIR_UPLOAD', 'Z:/home/e-shop/www/system/upload/');
+define('DIR_LOGS', 'Z:/home/e-shop/www/system/logs/');
+define('DIR_MODIFICATION', 'Z:/home/e-shop/www/system/modification/');
+define('DIR_CATALOG', 'Z:/home/e-shop/www/catalog/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
--- config.php
+++ config.php
@@ -1,22 +1,22 @@
 <?php
 // HTTP
-define('HTTP_SERVER', 'http://e-shop2/');
+define('HTTP_SERVER', 'http://e-shop/');
 
 // HTTPS
-define('HTTPS_SERVER', 'http://e-shop2/');
+define('HTTPS_SERVER', 'http://e-shop/');
 
 // DIR
-define('DIR_APPLICATION', 'Z:/home/e-shop2/www/catalog/');
-define('DIR_SYSTEM', 'Z:/home/e-shop2/www/system/');
-define('DIR_LANGUAGE', 'Z:/home/e-shop2/www/catalog/language/');
-define('DIR_TEMPLATE', 'Z:/home/e-shop2/www/catalog/view/theme/');
-define('DIR_CONFIG', 'Z:/home/e-shop2/www/system/config/');
-define('DIR_IMAGE', 'Z:/home/e-shop2/www/image/');
-define('DIR_CACHE', 'Z:/home/e-shop2/www/system/cache/');
-define('DIR_DOWNLOAD', 'Z:/home/e-shop2/www/system/download/');
-define('DIR_UPLOAD', 'Z:/home/e-shop2/www/system/upload/');
-define('DIR_MODIFICATION', 'Z:/home/e-shop2/www/system/modification/');
-define('DIR_LOGS', 'Z:/home/e-shop2/www/system/logs/');
+define('DIR_APPLICATION', 'Z:/home/e-shop/www/catalog/');
+define('DIR_SYSTEM', 'Z:/home/e-shop/www/system/');
+define('DIR_LANGUAGE', 'Z:/home/e-shop/www/catalog/language/');
+define('DIR_TEMPLATE', 'Z:/home/e-shop/www/catalog/view/theme/');
+define('DIR_CONFIG', 'Z:/home/e-shop/www/system/config/');
+define('DIR_IMAGE', 'Z:/home/e-shop/www/image/');
+define('DIR_CACHE', 'Z:/home/e-shop/www/system/cache/');
+define('DIR_DOWNLOAD', 'Z:/home/e-shop/www/system/download/');
+define('DIR_UPLOAD', 'Z:/home/e-shop/www/system/upload/');
+define('DIR_MODIFICATION', 'Z:/home/e-shop/www/system/modification/');
+define('DIR_LOGS', 'Z:/home/e-shop/www/system/logs/');
 
 // DB
 define('DB_DRIVER', 'mysqli');
